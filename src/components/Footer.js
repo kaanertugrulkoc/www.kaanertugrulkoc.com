6cedb68a5fd1bd1d228240520d41c4c795c4ceeb
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-15 21:37:48 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 Kaan Ertuğrul Koç. Tüm hakları saklıdır.</p>
+        </footer>
+    );
+};
+
+export default Footer;
