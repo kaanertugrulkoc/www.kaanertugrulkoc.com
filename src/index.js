cafc5fa9e6cad01e138025b42c638cc8423a1021
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-15 21:44:32 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="hakkında" element={<About />} />
+        <Route path="iletişim" element={<Contact />} />
+      </Route>
+    </Routes>
+  </Router>,
+  document.getElementById("root")
+);
