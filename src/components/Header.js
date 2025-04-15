9f47b2ab180e9ad8a92a31bdb421d1c8e6db5856
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-15 21:37:06 +0300

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Kaan Ertuğrul Koç</h1>
+            <nav>
+                <ul>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#videos">Eğitim Videoları</a></li>
+                    <li><a href="#documents">Dökümanlar</a></li>
+                    <li><a href="#tools">Araçlar</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
