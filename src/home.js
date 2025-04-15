0392797ffe49749002e77a9dd7647bcffb2e7aa1
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-15 21:42:35 +0300

    Create home.js

---
+++ src/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Hoş Geldiniz!</h2>
+      <p>Bu benim kişisel web sitem. Blog yazılarımı, eğitim videolarımı ve dökümanlarımı paylaşıyorum.</p>
+    </div>
+  );
+};
+
+export default Home;
