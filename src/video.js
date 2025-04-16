ee0a5d257275442f6cfd78a5b3186cf773910082
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-16 18:48:31 +0300

    Create video.js

---
+++ src/video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Videos = () => {
+    return (
+        <div>
+            <h2>Eğitim Videoları</h2>
+            {/* Video listesi buraya gelecek */}
+        </div>
+    );
+};
+
+export default Videos;
