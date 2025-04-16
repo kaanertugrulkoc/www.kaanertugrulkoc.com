7e5c4bf6821fbc431ae7f8bbe364cf7d174c5a52
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-16 21:02:05 +0300

    Create biografi.js

---
+++ src/biografi.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Biography = () => {
+    return (
+        <div>
+            <h2>Biografi</h2>
+            <p>Biografi content...</p>
+        </div>
+    );
+};
+
+export default Biografi;
