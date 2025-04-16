9aa6a95918008e47a2bfcc319452d2f7ec67963d
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-16 20:08:09 +0300

    Create blog.js

---
+++ src/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h2>Blog</h2>
+            {/* Blog yazıları buraya gelecek */}
+        </div>
+    );
+};
+
+export default Blog;
