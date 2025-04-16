70a67dbf093dcf314d553141d279b12337586221
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-16 20:10:54 +0300

    Create egitimler.js

---
+++ src/egitimler.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Egitimler = () => {
+    return (
+        <div>
+            <h2>Eğitim Videoları</h2>
+            {/* Eğitim videoları buraya gelecek */}
+        </div>
+    );
+};
+
+export default Egitimler;
