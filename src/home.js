ccdcefe8108689c30b9f62a450984ebcc24607d2
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-20 19:59:30 +0300

    Update home.js

--- src/home.js
+++ src/home.js
@@ -6,6 +6,7 @@ const Home = () => {
       <h2>Hoş Geldiniz!</h2>
       <p>Bu benim kişisel web sitem. Blog yazılarımı, eğitim videolarımı ve dökümanlarımı paylaşıyorum.</p>
       <p>Yazmış olduğum yazılımlarıda yazılımlar sayfasından inceleyebilirsiniz.</p>
+    
     </div>
   );
 };
