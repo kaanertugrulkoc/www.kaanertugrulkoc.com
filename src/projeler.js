e2c7ae79b651ba4d7f17393817f60fad295ffdf1
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-25 18:49:24 +0300

    Create projeler.js

---
+++ src/projeler.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Egitimler = () => {
+    return (
+        <div>
+            <h2>Projeler</h2>
+            {/* projelerim buraya gelecek */}
+        </div>
+    );
+};
+
+export default Projeler;
